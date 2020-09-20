5c0f6e818ec96a8a8fa2d63de70a55fdf1ac93d8
Amardeep Kumar <amardeepkumar1205@gmail.com>
2020-09-21 04:06:37 +0530

    historical graph completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 
-const App = () => {
+import Historical from "./container/Historical/Historical";
+import Live from "./container/Live/Live";
+
+const App = (props) => {
+  const { history } = props;
   return (
-    <div>
-      <h1>Stocks</h1>
-    </div>
+    <>
+      <AppBar position="sticky" color="primary">
+        <Toolbar>
+          <Typography variant="h6" onClick={() => history.push("/")}>
+            Stocks
+          </Typography>
+          <Button color="inherit" onClick={() => history.push("/live")}>
+            Live
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/live" component={Live} />
+        <Route path="/" excat component={Historical} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 };
 
-export default App;
+export default withRouter(App);
